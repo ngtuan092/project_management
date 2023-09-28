2785eec9a2b2529f8028067b7c0115d6a86b7794
Thu Ha <dinh.thi.thu.ha@sun-asterisk.com>
2023-09-28 16:42:17 +0700

    delete_lesson_learn

--- app/controllers/lesson_learns_controller.rb
+++ app/controllers/lesson_learns_controller.rb
@@ -1,5 +1,7 @@
 class LessonLearnsController < ApplicationController
   before_action :logged_in_user
+  before_action :find_lesson_learn, only: %i(show destroy)
+  before_action :check_role, only: :destroy
 
   add_breadcrumb I18n.t("layouts.sidebar.lesson_learn"), :lesson_learns_path
 
@@ -12,5 +14,26 @@ class LessonLearnsController < ApplicationController
                      .by_recently_created
     @pagy, @lesson_learns = pagy @lesson_learns,
                                  items: Settings.pagy.number_items_10
+  end
+
+  def show; end
+
+  def destroy
+    if @lesson_learn.destroy
+      flash[:success] = t ".delete_success"
+      redirect_to lesson_learns_path
+    else
+      flash[:danger] = t ".fail_delete"
+      redirect_to lesson_learns_path, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def check_role
+    return if current_user.can_edit_delete_lesson_learn? @lesson_learn
+
+    flash[:warning] = t ".cannot_delete"
+    redirect_to lesson_learns_path
   end
 end
