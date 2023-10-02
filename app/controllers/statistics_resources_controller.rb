a4989ffd249239df0d5b682d8a8ff5a41e13d51b
Thu Ha <dinh.thi.thu.ha@sun-asterisk.com>
2023-10-02 17:39:35 +0700

    export statistical resources

--- app/controllers/statistics_resources_controller.rb
+++ app/controllers/statistics_resources_controller.rb
@@ -11,5 +11,20 @@ class StatisticsResourcesController < ApplicationController
                        .by_recently_created
     @pagy, @project_pagys = pagy @projects,
                                  items: Settings.pagy.number_items_10
+    @start_month_year = params[:start_month_year]
+    @end_month_year = params[:end_month_year]
+    respond
+  end
+
+  private
+  def respond
+    respond_to do |format|
+      format.html
+      format.xlsx do
+        date = Time.zone.now.strftime Settings.date.format
+        header = "attachment; filename=#{date}_statistics_resources.xlsx"
+        response.headers["Content-Disposition"] = header
+      end
+    end
   end
 end
--- app/helpers/statistics_resources_helper.rb
+++ app/helpers/statistics_resources_helper.rb
@@ -28,4 +28,54 @@ module StatisticsResourcesHelper
                                              .result
     resource_hash_to_html hash_month, :td
   end
+
+  def start_month_field_value month_year_params
+    return month_year_params if month_year_params
+
+    date = Date.current.change(month: 1)
+    date.strftime Settings.date.format_month_field
+  end
+
+  def month_number_displayed start_month_year, end_month_year
+    start_date = convert_month_year_to_date start_month_year
+    end_date = convert_month_year_to_date end_month_year
+    if start_date > end_date ||
+       (start_date.end_of_month > Date.current.end_of_month)
+      return []
+    end
+
+    end_date = Date.current if end_date > Date.current
+    (start_date..end_date).map{|d| [d.year, d.month]}.uniq
+  end
+
+  def convert_month_year_to_date month_year
+    [month_year, "1"].join("-").to_date
+  end
+
+  def resources_project_xlsx project, start_month_year, end_month_year
+    hash_month = ProjectMonthAnalyzer.call(project,
+                                           start_month_year, end_month_year)
+                                     .result
+    hash_month.values.map{|value| value[:resource]}
+  end
+
+  def resources_projects_xlsx projects, start_month_year, end_month_year
+    hash_month = ProjectsMonthSummaryAnalyzer.call(projects,
+                                                   start_month_year,
+                                                   end_month_year)
+                                             .result
+    values_array = hash_month.values.flat_map{|value| value[:resource]}
+    values_array.unshift I18n.t("value_resources.index.total")
+  end
+
+  def header_months_xlsx start_month_year, end_month_year
+    header = []
+    header << I18n.t("projects.index.name")
+
+    month_number_displayed(start_month_year, end_month_year).each do |i|
+      month = "#{i[1]} - #{i[0]}"
+      header << "#{I18n.t('project_features.project_feature.month')} #{month}"
+    end
+    header << I18n.t("value_resources.index.total_year")
+  end
 end
