f4951fbe9a840c1ab782447af39a316825a64176
Thu Ha <dinh.thi.thu.ha@sun-asterisk.com>
2023-10-04 00:58:33 +0700

    show_statistic_value

--- app/controllers/statistics_resources_controller.rb
+++ app/controllers/statistics_resources_controller.rb
@@ -7,7 +7,7 @@ class StatisticsResourcesController < ApplicationController
   def index
     @projects = Project.filter_project(params[:project_id])
                        .filter_group(params[:group_id])
-                       .filter_status(list_statuses_selected)
+                       .filter_status(params[:status])
                        .by_recently_created
     @pagy, @project_pagys = pagy @projects,
                                  items: Settings.pagy.number_items_10
---
+++ app/helpers/dashboard_values_helper.rb
@@ -0,0 +1,17 @@
+module DashboardValuesHelper
+  def value_project project, start_month_year, end_month_year
+    hash_month = project_month_service.call(project, start_month_year,
+                                            end_month_year).result
+    hash = {}
+    hash_month.each do |key, value|
+      hash[key] = value[:value]
+    end
+    {name: project.name, data: hash}
+  end
+
+  def values_chart projects, start_month_year, end_month_year
+    projects.map do |project|
+      value_project(project, start_month_year, end_month_year)
+    end
+  end
+end
